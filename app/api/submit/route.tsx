f9e7e7c0c4aafd9daba232824e50d2182e66de71
yunhong li <simon.yunhong@outlook.com>
2023-12-07 13:05:14 +1100

    create machine number based on user input and insert into database

--- app/api/submit/route.tsx
+++ app/api/submit/route.tsx
@@ -1,10 +1,22 @@
 import prisma from "../../../lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
- 
+
+const getSubmitedDate = function() {
+  return new Date().toLocaleDateString().replaceAll('/', '');
+}
+
+const getSerialNumber = function (length:number):number {
+  return Math.floor(Math.random() * length);
+}
+
 export async function POST(request: Request) {
 
   const formdata = await request.formData();
   const formdataJson = Object.fromEntries(formdata);
+
+  const submitedDate = getSubmitedDate()
+  const randomNumber = getSerialNumber(1000000000)
+  const machineNumber = `${formdataJson.model}_${formdataJson.quantity}_${submitedDate}_${randomNumber}`;
 
   const insertData = await prisma.batch.create({
     data: {
@@ -13,6 +25,7 @@ export async function POST(request: Request) {
       quantity: Number(formdataJson.quantity),
       license_level: Number(formdataJson.licenseLevel),
       comment_message: formdataJson.commentMessage.toString(),
+      machine_number: machineNumber.toString(),
     }
   })
 
